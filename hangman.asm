32910095d44ec4d5573354258ea91f9c62a25e5c
Mark Lohstroh <mlohstroh@gmail.com>
2013-09-19 14:48:03 -0500

    trying to work through looping the file

--- hangman.asm
+++ hangman.asm
@@ -14,11 +14,18 @@ loseMessage:
 #Addresses to hold info
 loadedWords: 
 	.space 1000
+storedWords
+	.space 1000 #totally unsure about the size for these...
+chosenWord:
+	.space 10
 
 .text
 # Main program starts here
 main:
 	jal loadWords
+	jal prepForCount
+	jal countWords
+	
 	jal promptUser
 	
 	li $v0, 10
@@ -29,6 +36,30 @@ promptUser:
 	la $a0, greeting
 	syscall
 	jr $ra
+
+prepForCount:
+	la $a0, loadedWords	
+	la $a1, storedWords
+	sw $a0, ($a1)
+	li $v0, 1
+	add $a1, $a1, 4
+	jr $ra
+
+#sets $s0 as the number of words in file
+countWords:
+	lb $t0, ($a0)
+	beq $t0, 0, finishCounting
+	add $v0, $v0, 1 #increment counter
+	add $a0, $a0, 1
+	sw $a0, ($a1)
+	add $a1, $a1, 4
+	j countWords
+	
+finishCounting:
+	li, $v0, 1
+	move $a0, $s0
+	syscall
+
 	
 loadWords:
 	li $v0, 13 #open file syscall
