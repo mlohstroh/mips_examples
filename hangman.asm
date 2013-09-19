2043af9a3a56dda2bb17fffd1bd698892ca3e41d
Mark Lohstroh <mlohstroh@gmail.com>
2013-09-19 14:25:14 -0500

    program compiles and actually prints a message now

--- hangman.asm
+++ hangman.asm
@@ -3,16 +3,32 @@
 #Strings
 wordsFile:
 	.asciiz "hangman_words/words.txt"
+greeting:
+	.asciiz "Welcome to Hangman. You must guess the word that was randomly selected from a dictionary"
+winMessage:
+	.asciiz "You won!"
+loseMessage:
+	.asciiz "You lost!"
+
 
 #Addresses to hold info
 loadedWords: 
 	.space 1000
 
+.text
+# Main program starts here
 main:
-	j loadWords
+	jal loadWords
+	jal promptUser
+	
+	li $v0, 10
+	syscall
 	
 promptUser:
-	
+	li $v0, 4
+	la $a0, greeting
+	syscall
+	jr $ra
 	
 loadWords:
 	li $v0, 13 #open file syscall
@@ -23,6 +39,13 @@ loadWords:
 	
 	move $s0, $v0 #file descriptor
 	li $v0, 14
+	move $a0, $s0
+	la $a1, loadedWords #address for loaded words
+	li, $a2, 1000 #buffer size
+	syscall
 	
+	li $v0, 16 #close file
+	move $a0, $s0
+	syscall
 	
-	j promptUser
+	jr $ra #just return
